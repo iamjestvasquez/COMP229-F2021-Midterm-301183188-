4f8970cbaf39bf7b637a0f4f9b660dd04c2cbf08
Jestoni Vasquez <jestvasquez.main@gmail.com>
2021-10-27 18:33:06 -0400

    Fixed Add button, Added Edit and Delete button

---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://jest-vasquez:EWUBor1O1uttNaW2@cluster0.zcnp5.mongodb.net/book_store_mtest?retryWrites=true&w=majority",
+  "Secret": 'SomeSecret'
+};
---
+++ server/routes/books.js
@@ -0,0 +1,76 @@
+/**
+routes/books.js
+Name: Jestoni Vasquez
+Student ID: 301183188
+Web App Name: Booklist
+ */
+
+// modules required for routing
+let express = require('express');
+let router = express.Router();
+let mongoose = require('mongoose');
+
+// define the book model
+let book = require('../models/books');
+
+/* GET books List page. READ */
+router.get('/', (req, res, next) => {
+  // find all books in the books collection
+  book.find( (err, books) => {
+    if (err) {
+      return console.error(err);
+    }
+    else {
+      res.render('books/index', {
+        title: 'Books',
+        books: books
+      });
+    }
+  });
+
+});
+
+//  GET the Book Details page in order to add a new Book
+router.get('/details', (req, res, next) => {
+    /*****************
+     * ADD CODE HERE *
+     *****************/
+
+});
+
+// POST process the Book Details page and create a new Book - CREATE
+router.post('/details', (req, res, next) => {
+
+       /*****************
+     * ADD CODE HERE *
+     *****************/
+
+});
+
+// GET the Book Details page in order to edit an existing Book
+router.get('/edit/:id', (req, res, next) => {
+
+         /*****************
+     * ADD CODE HERE *
+     *****************/
+
+});
+
+// POST - process the information passed from the details form and update the document
+router.post('/edit/:id', (req, res, next) => {
+
+         /*****************
+     * ADD CODE HERE *
+     *****************/
+});
+
+// GET - process the delete by user id
+router.get('/delete/:id', (req, res, next) => {
+
+        /*****************
+     * ADD CODE HERE *
+     *****************/
+});
+
+
+module.exports = router;
